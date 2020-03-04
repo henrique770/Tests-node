3e4c5f26984b930bd8d1e0c82f0d65d16cb91a6f
henrique770 <henrique.1360@gmail.com>
2020-03-04 15:03:31 -0300

    test: duplicated email

--- __tests__/integration/user.test.js
+++ __tests__/integration/user.test.js
@@ -1,7 +1,12 @@
 import request from 'supertest';
 import app from '../../src/app';
 
+import truncate from '../util/truncate';
+
 describe('User', () => {
+  beforeEach(async () => {
+    await truncate();
+  });
   it('deve ser possível se cadastrar', async () => {
     const response = await request(app)
       .post('/users')
@@ -12,5 +17,24 @@ describe('User', () => {
       });
     // vai verificar se tem id
     expect(response.body).toHaveProperty('id');
+  });
+
+  it('não deve ser capaz de registrar email já criado ', async () => {
+    await request(app)
+      .post('/users')
+      .send({
+        name: 'Henrique Leal',
+        email: 'henrique.1360@gmail.com',
+        password_hash: '123456',
+      });
+    const response = await request(app)
+      .post('/users')
+      .send({
+        name: 'Henrique Leal',
+        email: 'henrique.1360@gmail.com',
+        password_hash: '123456',
+      });
+
+    expect(response.status).toBe(400);
   });
 });
